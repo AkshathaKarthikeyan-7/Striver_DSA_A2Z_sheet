a04faafc4aad56f9d7e938418ef067ccfa885c44
AkshathaKarthikeyan-7 <akshatha.karthikeyank@gmail.com>
2025-12-07 12:21:42 +0530

    Intial commit

---
+++ Strivers_DSA_A2Z_Sheet/src/Array/Linear_Search.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Linear_Search {
+    public static void main(String[] args){
+        int [] arr = {1,2,3,4,5};
+        int num = 3;
+        boolean found = false;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == num){
+                System.out.println(i);
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            System.out.print("Not Found");
+        }
+    }
+}
