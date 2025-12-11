934a4712cf0db9753c7c2d41d554405aa59736e4
AkshathaKarthikeyan-7 <akshatha.karthikeyank@gmail.com>
2025-12-11 09:03:52 +0530

    Intial commit

---
+++ Strivers_DSA_A2Z_Sheet/src/Array/Finding_Missing_Number.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Finding_Missing_Number {
+    public static void main(String[] args){
+        int [] arr = {1,2,4,5};
+        int n = 5;
+        int total = (n*(n+1))/2;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        int missing = total - sum;
+        System.out.print(missing);
+    }
+}
