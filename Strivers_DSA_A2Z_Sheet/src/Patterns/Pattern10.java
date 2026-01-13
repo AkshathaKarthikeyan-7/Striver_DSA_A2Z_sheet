efbc9a365a7ba1d72e4b90cc7a0148db0d3139e3
AkshathaKarthikeyan-7 <akshatha.karthikeyank@gmail.com>
2026-01-13 11:12:37 +0530

    Intial commit

---
+++ Strivers_DSA_A2Z_Sheet/src/Patterns/Pattern10.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int stars = i;
+
+            if (i >= n) {
+                stars = 2 * n - i - 2;
+            }
+
+            for (int j = 0; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
