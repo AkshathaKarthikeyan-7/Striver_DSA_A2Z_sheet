36b5dd461e5ac551617a57a82e78fbe0f8933425
AkshathaKarthikeyan-7 <akshatha.karthikeyank@gmail.com>
2026-01-19 17:26:06 +0530

    Intial commit

--- Strivers_DSA_A2Z_Sheet/src/Basic_Math/Print_All_Divisors.java
+++ Strivers_DSA_A2Z_Sheet/src/Basic_Math/Print_All_Divisors.java
@@ -1,11 +1,16 @@
 package Basic_Math;
 
+import static java.lang.Math.sqrt;
+
 public class Print_All_Divisors {
     public static void main(String[] args) {
         int n = 36;
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= sqrt(n); i++){
             if(n%i == 0){
-                System.out.print((i)+" ");
+                System.out.print(i+" ");
+                if((n/i) != i){
+                    System.out.print(n/i+" ");
+                }
             }
         }
     }
