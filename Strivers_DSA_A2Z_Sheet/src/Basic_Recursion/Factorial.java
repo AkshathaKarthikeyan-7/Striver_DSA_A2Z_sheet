03255c30e6c2ffee3cdafb5ae63d422c4089e191
AkshathaKarthikeyan-7 <akshatha.karthikeyank@gmail.com>
2026-01-21 18:27:36 +0530

    Intial commit

---
+++ Strivers_DSA_A2Z_Sheet/src/Basic_Recursion/Factorial.java
@@ -0,0 +1,13 @@
+package Basic_Recursion;
+
+public class Factorial {
+    public static void main(String[] args){
+        int n = 5;
+        int result = factorial(n);
+        System.out.print(result);
+    }
+    public static int factorial(int n){
+        if(n == 0 || n == 1) return 1;
+        return n*factorial(n-1);
+    }
+}
